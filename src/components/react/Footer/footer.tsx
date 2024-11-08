2366b3e4e5c99ae763d7166c01d985687e22fa46
AthulBiju11 <athulbijuts@gmail.com>
2024-11-08 20:28:46 +0530

    Finished with footer component

---
+++ src/components/react/Footer/footer.tsx
@@ -0,0 +1,19 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-contents">
+        <div className="copyright-div">Copyright © 2024 TELE SYSTEMS - All Rights Reserved</div>
+        <div className="links-div">
+            <ul>
+                <li>About Us</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
