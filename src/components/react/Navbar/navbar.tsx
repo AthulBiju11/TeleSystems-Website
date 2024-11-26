caa93c5aedd1c8225ce2f5d1621cf4ea3791224c
AthulBiju11 <athulbijuts@gmail.com>
2024-11-26 18:45:56 +0530

    Changed the logo  and added minor changes

--- src/components/react/Navbar/navbar.tsx
+++ src/components/react/Navbar/navbar.tsx
@@ -1,45 +1,57 @@
-
-
 import { useState } from "react";
 import "./navbar.scss";
 import ViewTransitions from "astro/components/ViewTransitions.astro";
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  
+
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
   return (
     <nav>
-      <div className={`navbar-wrapper ${isMenuOpen ? 'expanded' : ''}`}>
+      <div className={`navbar-wrapper ${isMenuOpen ? "expanded" : ""}`}>
         <div className="navbar-container">
           <div className="logo-container">
-            <div className="logo"><a href="/">Tele Systems</a></div>
+            <div className="logo">
+              <a className="small-screen-logo" href="/">
+                Tele Systems
+              </a>
+              <a className="main-logo-title" href="/">
+                Tele Systems Telephone Shop
+                <div className="logo-sub-title">TELEPHONES EXCLUSIVE</div>
+              </a>
+            </div>
           </div>
           <div className="navbar-options">
             <button className="menu-button" onClick={toggleMenu}>
               {isMenuOpen ? "X" : "☰"}
             </button>
-            
+
             <div className="menu">
               <ul>
-                <li><a href="/products/">All Products</a></li>
-                <li><a href="/contact-us/">Contact Us</a></li>
+                <li>
+                  <a href="/products/">All Products</a>
+                </li>
+                <li>
+                  <a href="/contact-us/">Contact Us</a>
+                </li>
               </ul>
             </div>
           </div>
         </div>
         <div className="mobile-menu">
           <ul>
-            <li><a href="/products/">All Products</a></li>
-            <li><a href="/contact-us/">Contact Us</a></li>
+            <li>
+              <a href="/products/">All Products</a>
+            </li>
+            <li>
+              <a href="/contact-us/">Contact Us</a>
+            </li>
           </ul>
         </div>
       </div>
-      
     </nav>
-    
   );
 };
 
